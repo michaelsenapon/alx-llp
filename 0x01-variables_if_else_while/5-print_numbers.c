de18cdc13864b03ccae7f5b2e736cc4b02014b2d
michaelsenapon <michaelsenapon@gmail.com>
2022-03-11 21:02:08 +0000

    print 0 to 9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main- Print single digit numbers of base 10
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int d = 0;
+
+	do
+	{
+	printf("%d", d);
+	d++;
+	} while(d < 10);
+	printf("\n");
+
+	return (0);
+}
