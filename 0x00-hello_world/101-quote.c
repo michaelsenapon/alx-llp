519d8475ffb63fa1a466e23d462843a79d1a5255
michaelsenapon <michaelsenapon@gmail.com>
2022-03-14 00:04:02 +0000

    print strings to standard error without using printf or puts

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,6 +1,6 @@
 /*
  * File: 101-quote.c
- * Auth: Brennan D Baraban
+ * Auth: Michael BOJERENU
  */
 
 #include <unistd.h>
@@ -9,7 +9,7 @@
  * main - Prints "and that piece of art is useful" - Dora Korpar, 2015-10-19",
  *                followed by a new line, to standard error.
  *
- * Return: Always 1.
+ * Return: Always (1)
  */
 int main(void)
 {
