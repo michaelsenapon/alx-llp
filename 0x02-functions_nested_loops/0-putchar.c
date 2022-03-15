cdcc566c9b64e3af3a08b619bf3cdbbb24c606f3
michaelsenapon <michaelsenapon@gmail.com>
2022-03-15 16:33:32 +0000

    print _putchar using _putchar function

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,6 @@
 #include <unistd.h>
 #include "main.h"
 
-/**
- * Initial Description: This program prints _putchar using
- * _putchar function created locally.
- */
-
-/*
- * Function:  declaration
- *
- * Parameter C - This parameter represent character
- *
- * Return: On Success 1
- */
-int _putchar(char c); /* This function output single character */
-
 
 /**
  * Main: Prints _putchar, followed by new line
