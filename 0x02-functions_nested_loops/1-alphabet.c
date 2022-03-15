9c75214a59627e318dbc620e130956efb378d4bf
michaelsenapon <michaelsenapon@gmail.com>
2022-03-15 18:25:39 +0000

    edit print_alphabet file

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -23,8 +23,8 @@ int main(void)
 
 void print_alphabet(void)
 {
-char i;
-for (i = 'a'; i <= 'z'; i++);
-	_putchar('i');
-	return;
+	char i;
+	for (i = 'a'; i <= 'z'; i++);
+		_putchar('i');
+		return;
 }
