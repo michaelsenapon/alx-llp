9063352126358b919176f6d0e6233489fb6e76e3
michaelsenapon <michaelsenapon@gmail.com>
2022-03-15 15:20:30 +0000

    print _putchar using _putchar function

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,1 +1,44 @@
+/**
+ * Task 0: Prints _putchar, followed by a new line.
+ *
+ * Author: Michael BOJERENU
+ */
 
+/* Header files declaired */
+
+#include <unistd.h>
+
+/* Function declaration*/
+
+int _putchar(char c);
+
+/**
+ * Main: Prints _putchar, followed by new line
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+}
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
