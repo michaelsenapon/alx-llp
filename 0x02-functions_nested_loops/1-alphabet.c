e700efca3f59bcea10980df7a37a386a21e3f7ea
michaelsenapon <michaelsenapon@gmail.com>
2022-03-15 13:58:23 +0000

    sketch work

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,25 @@
+/**
+  *Task 1: Print the Alphabet in lowercase followed
+  *by new line
+  *Auth: Michael BOJERENU
+  */
+
+/*Header files*/
+#include "main.h"
+#include "putchar.c"
+
+/*Prototype of functions declared*/
+void print_alphabet(void);
+
+/**
+ * Main - Print alphabets in lowercase
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
+print_alphabet(
+
