95948a0c7224dadd2027fb57bb71111e13f2fe92
michaelsenapon <michaelsenapon@gmail.com>
2022-03-15 23:55:54 +0000

    check for lowercase character

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,9 +3,6 @@
 /**
  * int _islower - Checks for lowercase character
  * @c: Is a variable representing ascii values for lower
- * case characters
- *
- * Return 0 if failed
  *
  * Return 1 if success otherwise 0
  */
