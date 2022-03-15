d399fd4f372bc5ae54a9e19198ce21162f3f0b7d
michaelsenapon <michaelsenapon@gmail.com>
2022-03-15 20:23:21 +0100

    corrected print alphabetfile

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -9,17 +9,17 @@
 
 
 /**
- * print_alphabet - Print alphabets in lower case 
+ * print_alphabet - Print alphabets in lower case
  *
  * Return: void
  */
 void print_alphabet(void)
 {
 	char i;
+
 	for (i = 'a'; i <= 'z'; i++)
 	{
 		_putchar(i);
 	}
 	_putchar('\n');
-	return;
 }
