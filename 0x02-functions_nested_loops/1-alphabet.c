70846857b07012ceee2b28bee5db913808bf5d61
michaelsenapon <michaelsenapon@gmail.com>
2022-03-15 20:07:31 +0100

    corrected print alphabetfile

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -20,11 +20,10 @@ int main(void)
 	return (0);
 }
 
-void print_alphabet(void);
+void print_alphabet(void)
 {
 char i;
-for (i = 'a'; i <= 'z'; i++);
-
+for (i = 'a'; i <= 'z'; i++)
 	_putchar('i');
 	return;
 }
