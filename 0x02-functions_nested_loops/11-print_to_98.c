8fd536b87c36fb73de8846764a781271a5ace51d
michaelsenapon <michaelsenapon@gmail.com>
2022-03-16 03:08:20 +0000

    print ascending or decending number to 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_to_98 - Print numbers from n to 98
+ * @n: n represent the beginning of prints
+ *
+ * Return: Always 0
+ */
+void print_to_98(int n)
+{
+	if (n <= 98)
+		for (i = n; i <= 98; i++)
+			printf("%d, ", i);
+			return;
+	else
+		for (i = n; i >=98; i==)
+			printf("%d, ", i);
+			return;
+}
