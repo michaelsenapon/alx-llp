aded64b21063e8906505857b7b63bfbd8a58f394
michaelsenapon <michaelsenapon@gmail.com>
2022-03-16 02:40:18 +0000

    addition of integers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add: - Print absolute value of an intege
+ * @c: Represent integers
+ *
+ * Return: Always 0
+ */
+int add(int a, int b)
+{	
+	int sum;
+	sum = (a + b);
+	return (sum);
+}
