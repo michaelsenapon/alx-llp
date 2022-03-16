a8484406709318b27b934e2cd307417496e7bdd5
michaelsenapon <michaelsenapon@gmail.com>
2022-03-16 02:00:49 +0000

    print last digit of integers

---
+++ 0x02-functions_nested_loops/7-main.c
@@ -0,0 +1,1 @@
+int print_last_digit(int c);
--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,22 +1,15 @@
 #include "main.h"
 
 /**
- * print_last_digit: - Print absolute value of an integer
- * @int: Represent integers
+ * print_last_digit: - Print absolute value of an intege
+ * @c: Represent integers
  *
  * Return: Always 0
  */
-int print_last_digit(int a)
+int print_last_digit(int c)
 {	
-	int a;
-	if (a < 0)
-	{
-		int result;
-		result = (a * (-1));
-		return (result);
-	}
-	else
-	{
-		return (a);
-	}
+	int last_digit = (c % 10)
+
+	return (last_digit);
+
 }
