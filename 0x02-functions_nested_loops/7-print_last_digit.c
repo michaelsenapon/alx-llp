7d265d9a7b6062fbe3f22e3dd5d3a4c63a778d3c
michaelsenapon <michaelsenapon@gmail.com>
2022-03-16 02:13:35 +0000

    print last digit of integers

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -9,7 +9,12 @@
 int print_last_digit(int c)
 {	
 	int last_digit = (c % 10);
-
-	return (last_digit);
+	
+	if (last_digit >= 0)
+	{
+		return (last_digit);
+	}
+	else if (last_digit < 0)
+		last_digit = (last_digit * (-1);
 
 }
