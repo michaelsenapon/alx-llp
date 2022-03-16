de6327d7248d500ffd9d770bd755740899d92ca3
michaelsenapon <michaelsenapon@gmail.com>
2022-03-16 01:12:20 +0000

    print absolute values of integers

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _abs - Print absolute value of an integer
+ * @int: Represent integers
+ *
+ * Return: Success (0)
+ */
+int _abs(int)
+{	
+	int n;
+	if (n < 0)
+	{
+		n = (n * (-1));
+		return (0);
+	}
+	else
+	{
+		n = n;
+		return (0);
+	}
+}
