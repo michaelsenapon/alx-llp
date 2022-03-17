d8fd1f7a899ea6e47b6962a269983fdb3fe963c7
michaelsenapon <michaelsenapon@gmail.com>
2022-03-17 18:18:40 +0000

    corrected _isupper function

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -7,14 +7,11 @@
  * Return: 1 if c is uppercase and 0 if otherwise
  */
 
-int _isupper(int c);
+int _isupper(int c)
 {
 	if (c < 65 || c > 90)
-	{
 		return (0);
-	}
 	else
-	{
 		return (1);
-	}
+
 }
