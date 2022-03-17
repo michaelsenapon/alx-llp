9062a76cedfd4828b6d7ba9149d7aee3d6338a4b
michaelsenapon <michaelsenapon@gmail.com>
2022-03-17 18:34:56 +0000

    conform to betty style

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- * is_digit - This function checks for a digit (0 through 9)
+ * _isdigit - checks for digits 0 - 9
  * @c: represent the digit
  *
- * Return: return value is 1 if c is digit and 0 if otherwise
+ * Return: success value is 1 if c is digit and 0 if otherwise
  */
 int _isdigit(int c)
 {
