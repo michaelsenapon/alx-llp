9e177b3afe34c75ad074536349b466ab1d31f322
michaelsenapon <michaelsenapon@gmail.com>
2022-03-17 20:11:33 +0000

    print 0-14 ten times

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * more_numbers - Function print no 0 - 14 ten times and new line
+ *
+ * Return: void
+ */
+void more_numbers(void)
+{
+	int n;
+	for (n = 0; n < 10; n++)
+	{
+		int i;
+		for (i = 0; i < 15 i++)
+		{
+			_putchar(i + '0');
+		}
+
+		_putchar('\n')
+	}
+}
