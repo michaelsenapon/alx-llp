b90afd43298d27aa2bc3f985d9b3d6b616126370
michaelsenapon <michaelsenapon@gmail.com>
2022-03-17 08:02:22 +0000

    edited main.h file

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,3 +1,7 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+
 int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
@@ -10,3 +14,5 @@ int add(int a, int b);
 void print_to_98(int n);
 void jack_bauer(void);
 void times_table(void);
+
+#endif 
