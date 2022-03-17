15e182b5bf3b22313c468f28be82e3e83f51c995
michaelsenapon <michaelsenapon@gmail.com>
2022-03-17 19:06:17 +0000

    print digits from 0 to 9 usuing putchar function

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_numbers - Function print numbers from 0 to 9 and new line
+ *
+ * Return: void
+ */
+void print_numbers(void)
+{
+	int n;
+
+	for (n = 0; n <= 9; n++)
+	{
+		_putchar(n);
+	}
+	_putchar('\n');
