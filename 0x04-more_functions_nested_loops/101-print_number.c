55f23b8753c80e0465220a6907cbf8d31c52d1ad
michaelsenapon <michaelsenapon@gmail.com>
2022-03-18 03:56:50 +0000

    print numbers with restrictions

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,28 +1,21 @@
-#include "main..h"
-#include <stdio.h>
-
+#include "main.h"
 /**
-  * print_number - Prints an integer.
-  * @n: The integer to prints.
-  *
-  * Return: Nothing!
-  */
+ *print_number - print integer.
+ *@n: integer.
+ *
+ *Return: void.
+ */
 void print_number(int n)
 {
-	unsigned int k = n;
+	unsigned int x;
 
+	x = n;
 	if (n < 0)
 	{
-		n *= -1;
-		k = n;
-		_putchar('-');
+		_putchar(45);
+		x = -n;
 	}
-
-	k /= 10;
-
-	if (k != 0)
-		print_number(k);
-
-	_putchar((unsigned int) n % 10 + '0');
-
+	if (x / 10)
+		print_number(x / 10);
+	_putchar((x % 10) + '0');
 }
