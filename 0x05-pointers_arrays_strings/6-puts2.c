7ba0aea8691be7c5eb832fbca408bba16182f733
michaelsenapon <michaelsenapon@gmail.com>
2022-03-21 23:32:44 +0000

    print every other string starting from first

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -2,13 +2,14 @@
 
 /**
  * puts2 - print every other character of a string and a new line.
- * @str is a pointer to the string to be printed
+ * @str: is a pointer to the string to be printed
  *
  * Return nothing.
  */
 void puts2(char *str)
 {
 	int i = 0;
+
 	while (str[i] != '\0')
 	{
 		if (i % 2 == 0)
