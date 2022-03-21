f882b6cd4f025ff52129a05f3ca502d8104365f1
michaelsenapon <michaelsenapon@gmail.com>
2022-03-21 22:33:07 +0000

    print string in reverse order

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_rev - print string in reverse order followed by a new line
+ * @s: is the pointer to the string to be printed
+ *
+ * Return: nothing
+ */
+void print_rev(char *s)
+{
+	while (*s)
+		_putchar(s*--);
+	_putchar('\n');
+}
