2d8dd3d364fdb01a2ada10085bc9467090144377
michaelsenapon <michaelsenapon@gmail.com>
2022-03-21 22:50:15 +0000

    print string in reverse order

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -8,7 +8,17 @@
  */
 void print_rev(char *s)
 {
-	while (*s)
-		_putchar(*s--);
+	int len = 0, index;
+
+	while (s[index++])
+		len++;
+
+	for (index = len - 1; index >= 0; index--)
+		_putchar(s[index]);
+
 	_putchar('\n');
 }
+
+
+
+
