a016cab8ca2fe8526522786f121ea13bf87b2ca8
michaelsenapon <michaelsenapon@gmail.com>
2022-03-21 21:23:37 +0000

    print string on stdout

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -2,13 +2,14 @@
 
 /**
  * _strlen - function that prints the length of strings
- * @s: is a pointer to the string 
+ * @s: is a pointer to the string
  *
  * Return: Always 0.
  */
 int _strlen(char *s)
 {
 	int i;
+
 	while (*s++)
 		i++;
 	return (i);
