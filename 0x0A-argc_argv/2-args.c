93e06f44de0e195e960bbae537905bb6d787e0ac
michaelsenapon <michaelsenapon@gmail.com>
2022-04-01 20:19:54 +0000

    program that prints all arguments it receives

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - program that prints all arguments it receives
+ * @argc: is unused and suppressed
+ * @argv: is pointer to the strings of arguments passed into program
+ *
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+	for (i = 0; i < argc; i++)
+		printf("%s\n", argv[i]);
+	return (0);
+}
