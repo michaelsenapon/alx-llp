9a36f774886341bea1a9207487eef9dcd40c00ae
michaelsenapon <michaelsenapon@gmail.com>
2022-04-05 23:12:55 +0000

    function that prints arrays

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * create_array - function that creates an array of chars
+ * @size: size
+ * @c: is the char
+ *
+ * Return: Null or Success
+ */
+char *create_array(unsigned int size, char c)
+{
+	unsigned int i;
+	char *m;
+
+	m = malloc(sizeof(char) * (size + 1));
+	if (size == 0)
+	{
+		return (NULL);
+	}
+	i = 0;
+	while (i < size)
+	{
+		m[i] = c;
+		i++;
+	}
+	m[i] = '\0';
+	return (m);
+}
