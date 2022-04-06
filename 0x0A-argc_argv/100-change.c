463e46ba5dedea13be57afc2b2661f362eae2894
michaelsenapon <michaelsenapon@gmail.com>
2022-04-06 18:43:55 +0000

    program that prints the minimum number of coins to make change for an amount of money

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -3,7 +3,7 @@
 #include <stdlib.h>
 
 /**
- * main - program that prints minimum number of coins to make change for an 
+ * main - program that prints minimum number of coins to make change for an
  * amount of money.
  * @argc: count of arguments passed when running the program
  * @argv: a pointer to the array of strings passed when running the program
@@ -12,15 +12,11 @@
  */
 int main(int argc, char *argv[])
 {
-	int divisors, cents;
-	int count = 0;
-	char *c;
+	int cents, count = 0;
 
 	if (argc == 2)
 	{
-		c = argv[1];
-		cents = atoi(c);
-
+		cents = atoi(argv[1]);
 		if (cents < 0)
 		{
 			printf("%d\n", 0);
@@ -29,29 +25,27 @@ int main(int argc, char *argv[])
 		if (cents >= 25)
 		{
 			count += (cents / 25);
-			cents%= 25;
+			cents %= 25;
 		}
 		if (cents >= 10)
 		{
-			count+= (cents / 10);
-			cents%= 10;
+			count += (cents / 10);
+			cents %= 10;
 		}
 		if (cents >= 5)
 		{
-			count+= (cents / 5);
-			cents%= 5;
+			count += (cents / 5);
+			cents %= 5;
 		}
 		if (cents >= 2)
 		{
-			count+= (cents / 2);
-			cents%= 2;
+			count += (cents / 2);
+			cents %= 2;
 		}
 		if (cents >= 0)
-		{
-			count+= cents;
+			count += cents;
 			printf("%d\n", count);
 			return (0);
-		}
 	}
 	else
 	{
