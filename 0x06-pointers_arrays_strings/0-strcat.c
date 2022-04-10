ffb14cd446feac670ca2eb3c48bc37ea00e9a48b
michaelsenapon <michaelsenapon@gmail.com>
2022-04-10 15:12:04 +0000

    make file Betty compliant

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,17 +1,24 @@
 #include "main.h"
 
+/**
+ * _strcat - a function that concatenate two strings
+ * @dest: is the destination of concatenate
+ * @src: is the string to be added to the other
+ *
+ * Return: a pointer to the resulting string dest
+ */
 char *_strcat(char *dest, char *src)
 {
-    int i, j;
+	int i, j;
 
-    for (i = 0; dest[i] != '\0'; i++)
-    ;
+	for (i = 0; dest[i] != '\0'; i++)
+		;
 
-    for (j = 0; src[j] != '\0'; j++)
-    {
-        dest[i] = src[j];
-        i++;
-    }
-    dest[i] = '\0';
-    return (dest);
+	for (j = 0; src[j] != '\0'; j++)
+	{
+		dest[i] = src[j];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
 }
