e8588a03f2abed27d9e1a0eefca541686225a3b6
michaelsenapon <michaelsenapon@gmail.com>
2022-04-12 03:51:58 +0000

    type structure defined in header file

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - a structure type with elements defined
+ * @name: pointer to element that hold dog's name
+ * @age: element that holds age
+ * @owner: pointer to element that holds owner's name
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* DOG_H */
