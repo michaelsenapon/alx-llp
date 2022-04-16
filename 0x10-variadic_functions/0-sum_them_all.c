f75fc2145f2ac1dd1fce510214746dcc62b52838
michaelsenapon <michaelsenapon@gmail.com>
2022-04-16 21:25:20 +0000

    variadic function that add figures

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdarg.h>
+
+/**
+ * sum_them_all - ...
+ * @n: number of arguments to be added
+ * @...: elpsis of variadic function
+ *
+ * Return: 0 or sum accordinly
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list figures;
+	int i, sum = 0;
+
+	if (n == 0)
+		return (0);
+	va_start(figures, n);
+	
+	for (i = 0; i < n; i++)
+		sum+= va_arg(figures, int);
+	va_end(figures);
+	return (sum);
+}
