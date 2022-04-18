77eb61fafd02c272ffb73b5b58f7a2064a04ef2f
michaelsenapon <michaelsenapon@gmail.com>
2022-04-18 13:50:35 +0000

    resolve syntax issue

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -16,9 +16,6 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	unsigned int i;
 	unsigned int num;
 
-	if (!separator)
-		return;
-
 	va_start(figures, n);
 
 	for (i = 0; i < n; i++)
