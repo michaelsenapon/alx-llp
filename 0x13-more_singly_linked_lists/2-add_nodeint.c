0cf0fa3917d1050c8f0cb0c613d4582c8f1e077f
michaelsenapon <michaelsenapon@gmail.com>
2022-04-25 22:07:21 +0000

    proper code indent for conditional statement

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -15,18 +15,18 @@ listint_t *add_nodeint(listint_t **head, const int n)
 	listint_t *newnode;
 
 	if (head != NULL)
-		{
-			newnode = malloc(sizeof(listint_t));
+	{
+		newnode = malloc(sizeof(listint_t));
 
-			if (newnode == NULL)
-				return (NULL);
-			{
-				newnode->n = n;
-				newnode->next = (*head);
-				(*head) = newnode;
-			}
-			return (newnode);
-		}
+		if (newnode == NULL)
+			return (NULL);
+
+		newnode->n = n;
+		newnode->next = (*head);
+		(*head) = newnode;
+	}
+		return (newnode);
+
 	return (0);
 }
 
