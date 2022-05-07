ebd23a85ec3ae6bc3fa3b2ad72045aae30a284d9
michaelsenapon <michaelsenapon@gmail.com>
2022-05-07 15:50:49 +0000

    [feat] correct

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -21,7 +21,9 @@ int create_file(const char *filename, char *text_content)
 	if (fd == -1)
 		return (-1);
 
-	write(fd, text_content, len);
+	if (text_content)
+		write(fd, text_content, len);
+
 	close(fd);
 	return (1);
 }
