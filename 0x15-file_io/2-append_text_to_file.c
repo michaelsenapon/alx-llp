b5442f7c3df1502751fc9b1c2b11923d60c1f2c3
michaelsenapon <michaelsenapon@gmail.com>
2022-05-07 16:25:47 +0000

    betty compliant

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -14,12 +14,12 @@ int append_text_to_file(const char *filename, char *text_content)
 
 	if (!filename)
 		return (-1);
-	
+
 	fd = open(filename, O_WRONLY | O_APPEND);
 
 	if (fd == -1)
 		return (-1);
-	
+
 	write(fd, text_content, strlen(text_content));
 	close(fd);
 	return (1);
