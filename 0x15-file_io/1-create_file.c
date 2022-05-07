f304b90cc39440707887ccd9753217529df4161e
michaelsenapon <michaelsenapon@gmail.com>
2022-05-07 14:10:51 +0000

    [feat] create empty file if text_content is NULL

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -16,13 +16,15 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
-
+	
 	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
 
 	if (fd == -1)
 		return (-1);
 
-	write(fd, text_content, len);
+	if (text_content)
+		write(fd, text_content, len);
+
 	close(fd);
 	return (1);
 }
