6828824779d61f7e932a64f3efc610be6b81eeef
michaelsenapon <michaelsenapon@gmail.com>
2022-05-07 13:50:06 +0000

    betty style compliant

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -5,6 +5,8 @@
  * create_file - function that creates a file
  * @filename: name of file to be created
  * @text_content: content to be written into file
+ *
+ * Return: 1 on success, -1 on failure
  */
 int create_file(const char *filename, char *text_content)
 {
