a489718d3eb4764635682f8cd17bdbf31fa2919a
michaelsenapon <michaelsenapon@gmail.com>
2022-05-07 14:18:14 +0000

    [feat] complete

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -16,14 +16,13 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
-	
-	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
 
 	if (fd == -1)
 		return (-1);
 
-	if (text_content)
-		write(fd, text_content, len);
+	write(fd, text_content, len);
 
 	close(fd);
 	return (1);
